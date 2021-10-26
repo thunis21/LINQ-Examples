ff4f56fa080325d54359d0ab24cb7a4ff2a3d15a
Thunis Kruger <thunis@firsthope.co.za>
2021-10-26 15:51:05 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+namespace LINQ_Examples
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+
+            // Example1();
+
+        }
+
+        //private static void Example1()
+        //{
+        //    //Datasource
+        //    int[] data = { 1, 2, 3, 4, 5, 6, 7 };
+
+        //    var qry=from result in data where result>4 select result;
+
+        //    foreach (var item in qry)
+        //    {
+        //        Console.WriteLine(item);
+        //    }
+
+        //}
+    }
+}
